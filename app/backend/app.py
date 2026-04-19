261e6ab2c86b887110c78423085e604f3882bf3b
jerome-dev26 <jdevmastet@gmail.com>
2026-04-19 10:53:22 +0300

    Update app.py

--- app/backend/app.py
+++ app/backend/app.py
@@ -1,14 +1,18 @@
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
+
 
 @app.route("/api/hello")
 def hello_world():
     return jsonify(message="Hello from SecureDock Backend!")
 
+
 @app.route("/api/status")
 def status():
     return jsonify(status="Backend is running.")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
